3356a2918f80fb71879dd85954f91bf2c9e9f927
Arnisfet <mrudge@student.21-school.ru>
2021-11-09 22:26:01 +0300

    Philosophers take a fork (dont tested)

--- src/actions.c
+++ src/actions.c
@@ -2,5 +2,25 @@
 
 void	put_fork(t_attribute *p)
 {
-	display_message(p, 1);
+	if ((p->number - p->data->philo) == 0)
+	{
+		pthread_mutex_lock(&p->data->forks[0]);
+		display_message(p, 1);
+		pthread_mutex_lock(&p->data->forks[p->number]);
+		display_message(p, 1);
+	}
+	if (p->number == 0)
+	{
+		pthread_mutex_lock(&p->data->forks[p->number]);
+		display_message(p, 1);
+		pthread_mutex_lock(&p->data->forks[0]);
+		display_message(p, 1);
+	}
+	else
+	{
+		pthread_mutex_lock(&p->data->forks[p->number]);
+		display_message(p, 1);
+		pthread_mutex_lock(&p->data->forks[p->number + 1]);
+		display_message(p, 1);
+	}
 }
